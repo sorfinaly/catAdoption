4be3a889f91caac36a03a88539c1e87495b2e996
anisfazlin <51455715+anisfazlin@users.noreply.github.com>
2022-01-13 00:45:24 +0800

    Update Donate.java

--- Donate.java
+++ Donate.java
@@ -5,7 +5,6 @@ import java.util.Optional;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
